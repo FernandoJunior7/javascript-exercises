10e52fbad2a6a1983b5a11200ef85533cafb9172
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-14 18:51:10 -0300

    Second exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTimes) {
+    let phrase = string;
 
+    if (numberOfTimes === 0) {
+      return '';
+    }
+
+    if (numberOfTimes < 0) {
+      return 'ERROR';
+    }
+
+    for (let i = 1; i < numberOfTimes; i++) {
+      phrase += string;
+    }
+
+    return phrase;
 };
 
 // Do not edit below this line
