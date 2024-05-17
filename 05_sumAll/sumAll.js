6ac444b3eaeb06c7d3afccce3a00ab4b743c0711
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-17 12:59:45 -0300

    Fifth exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  let sum = 0;
 
+  if (min < 0 || max < 0 ) return "ERROR";
+
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+
+  if (min > max) {
+    const tmp = max;
+    max = min;
+    min = tmp;
+  }
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+
+  return sum;
 };
 
 // Do not edit below this line
