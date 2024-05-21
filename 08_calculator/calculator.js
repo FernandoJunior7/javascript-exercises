9b524881a37722ecf77b029846940a5369429e99
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-21 10:28:01 -0300

    Eighth exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, current) => {
+    return total + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, current) => {
+    return total * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  }
+  if (number === 1) {
+    return number * 1;
+  }
+
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
