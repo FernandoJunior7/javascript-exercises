ad11e9c89236f890651ad54d830beb2228ea538a
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-21 13:35:02 -0300

    Nineth exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const stringWithoutPunctuation = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase();
+  const stringWithoutSpaces = stringWithoutPunctuation.split(' ').join('');
+  const charArray = stringWithoutSpaces.split('');
+  const reversedArray = charArray.toReversed();
+  const reversedString = reversedArray.join('');
+  return stringWithoutSpaces === reversedString;
 };
 
 // Do not edit below this line
