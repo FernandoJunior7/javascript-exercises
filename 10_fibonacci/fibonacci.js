7bec41ba912d2fb476b3f29b3eebaee8501b7eb3
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-21 13:49:09 -0300

    10th done yayy

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(nPos) {
+  nPos = Number(nPos);
 
+  if (nPos < 0) {
+    return 'OOPS';
+  }
+
+  if (nPos === 0) {
+    return 0;
+  }
+
+  if (nPos === 1 || nPos === 2) {
+    return 1;
+  }
+
+  return (fibonacci(nPos - 1) + fibonacci(nPos - 2));
 };
 
 // Do not edit below this line
