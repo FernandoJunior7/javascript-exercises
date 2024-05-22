f35b349b0b9f3a6661411a0e5be53dbedf8dd92b
FernandoJunior7 <fernando.junior1907@gmail.com>
2024-05-22 12:04:32 -0300

    Terminei saporra :tada

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const mappedPeople = people.map(person => {
+    if ('yearOfDeath' in person) {
+      person.livedYears = person.yearOfDeath - person.yearOfBirth;
+    }
 
+    if(!('yearOfDeath' in person)) {
+      const thisYear = new Date().getFullYear();
+      person.livedYears = thisYear - person.yearOfBirth;
+    }
+
+    return person;
+  });
+
+  const oldestPerson =  mappedPeople.sort((a, b) => b.livedYears - a.livedYears)[0];
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
